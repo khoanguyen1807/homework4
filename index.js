3f641f91ccd5cd080662b440dca208de4e5c0190
KhoaNguyenLe <18520925@gm.uit.edu.vn>
2021-11-07 16:50:29 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require('dotenv').config()
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+const swaggerUi = require("swagger-ui-express");
+const docs = require('./doc/swagger.json');
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(docs));
+
+// connect with database
+
+
+const MONGO_URI = `mongodb+srv://Nlk18072k:Nlk18072k@webproject.ybj3v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+mongoose.connect(MONGO_URI, () => {
+  console.log("Connected to DB");
+});
+
+app.use(cors());
+
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World, from express");
+});
+
+require("./app/routes")(app);
+
+app.listen(PORT, () =>
+  console.log(
+    `Hello world app listening on port ${PORT} with url http://localhost:${PORT}`
+  )
+);
